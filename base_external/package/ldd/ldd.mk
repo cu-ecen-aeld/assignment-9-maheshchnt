8d99eb0f339dd47db39147a7310aff5f9c979bc3
maheshchnt <maheshchnt@gmail.com>
2022-11-01 14:21:53 -0600

    commiting buildroot changes

--- base_external/package/ldd/ldd.mk
+++ base_external/package/ldd/ldd.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 7 git contents
-LDD_VERSION = 9be19e1d068d26121cd62275a9106f2461a9aa0c
+LDD_VERSION = e7ecc297f5fd1d74e026321db82f81ca5e1582f8
 
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
